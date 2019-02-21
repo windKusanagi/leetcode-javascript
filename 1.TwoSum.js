73b526c54b76bedab7a20fa6a7c4246554b75ee5
Xiang <xiang@bravenew.world>
2019-02-20 21:26:34 -0400

    Two Sum
    Submit js solution for the question Two Sum

---
+++ 1.TwoSum.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+var twoSum = function(nums, target) {
+    const dict = {};
+    for( let i=0; i< nums.length; i++){
+      let diff = target - nums[i];
+      if(dict[diff]!==undefined)
+        return [dict[diff], i]
+      dict[nums[i]] = i;
+    }
+  };
